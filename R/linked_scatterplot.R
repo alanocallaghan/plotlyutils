177719073cd6a8b6a59570bffa60bad4730a9f38
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2018-01-17 21:13:47 +0000

    Add travis, tests, coverage, more docs

--- R/linked_scatterplot.R
+++ R/linked_scatterplot.R
@@ -1,19 +1,26 @@
 #' linked_scatterplot
-#' 
+#'
 #' A scatterplot with links
-#' 
+#' @param x x values
+#' @param y y values
+#' @param groups Groups to color the points by.
+#' @param text Hovertext for the points
+#' @param title Plot title
+#' @param xlab,ylab X/Y axis title
+#' @param xlim,ylim Range of X/Y axis
+#' @param colors Colors to use for the groups
 #' @examples
 #' linked_scatterplot(
-#'  1:10, 
-#'  1:10, 
-#'  rep("https://www.google.com", 10), 
+#'  1:10,
+#'  1:10,
+#'  rep("https://www.google.com", 10),
 #'  groups=sample(letters[1:2], 10, replace=TRUE))
 #' @importFrom assertthat assert_that
 #' @export
-linked_scatterplot <- function(x, 
-        y, 
-        links, 
-        groups=rep("1", length(x)), 
+linked_scatterplot <- function(x,
+        y,
+        links,
+        groups=rep("1", length(x)),
         text = rep("", length(x)),
         title="",
         xlab="",
@@ -27,15 +34,15 @@ linked_scatterplot <- function(x,
         length(text) == length(y),
         length(colors) == length(unique(groups)))
 
-    createWidget("linked_scatterplot", 
-        x = list(x=x, 
+    createWidget("linked_scatterplot",
+        x = list(x=x,
             xlab=xlab,
             xlim=xlim,
-            y=y, 
+            y=y,
             ylab=ylab,
             ylim=ylim,
-            links=links, 
-            groups=groups, 
+            links=links,
+            groups=groups,
             colors=colors,
             title=title,
             text=text),
